bdfa42a25ef1fd744361facc02cce6e716072b12
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-05 21:35:52 -0800

    defined classes and functions required

--- subnet_calculator.py
+++ subnet_calculator.py
@@ -1,2 +1,20 @@
 # this program is written for sunetting calculator
 # CLASSROOM TASK 2
+
+
+
+def ipaddress():
+    pass
+
+
+def subnetmask():
+    pass
+
+
+
+def main():
+
+    ip=input("Enter ip address (seperated by dot):")
+
+if __name__ == '__main__':
+    main()
